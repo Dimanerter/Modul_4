b818cb28fc385b499a3eeabc9ffef7a9bf767f11
Dimanerter <erterujamp@gmail.com>
2024-09-29 16:41:41 +0200

    files

--- working_with_files/part1/main.py
+++ working_with_files/part1/main.py
@@ -1,4 +1,12 @@
-fh = open('test.txt', 'w')
-symbols_written = fh.write('hello!')
-print(symbols_written)
+
+
+fh = open('test.txt', 'w+')
+
+# symbols_written = fh.write('hello!')
+fh.write('hello!')
+fh.seek(0)
+# print(symbols_written)
+first_two_symbels = fh.read(2)
+print(first_two_symbels)
+
 fh.close()
