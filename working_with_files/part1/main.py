42c5ea49ce70955ee4fcb2e46d3fd85b3ca57a21
Dimanerter <erterujamp@gmail.com>
2024-09-29 17:50:38 +0200

    readline

--- working_with_files/part1/main.py
+++ working_with_files/part1/main.py
@@ -7,6 +7,25 @@ while True:
     symbol = fh.read(1)
     if len(symbol) == 0:
         break
-    print(symbol)
+    # print(symbol)
 
 fh.close()
+
+#-----------------------------readline-------------------------------
+
+fh = open('test.txt', 'w')
+fh.write('first line\nsecond line\nthird line')
+fh.close()
+
+
+fh = open('test.txt', 'r')
+while True:
+    line = fh.readline()
+    if not line:
+        break
+    print(line)
+
+
+    
+fh.close()
+
