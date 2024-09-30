50014a6e4aeb04bff31e656953c17cea4d854043
Dimanerter <erterujamp@gmail.com>
2024-09-30 19:32:02 +0200

    casefold

--- working_with_files/part1/main.py
+++ working_with_files/part1/main.py
@@ -1,3 +1,4 @@
+
 fh = open('test.txt', 'w')
 fh.write('hello!')
 fh.close()
@@ -128,3 +129,25 @@ byte_array = bytearray(b"Hello world!")
 string = byte_array.decode("utf-8")
 print(string)
 
+
+# Порівняння рядків
+
+string1 = "Hello World"
+string2 = "hello world"
+
+if string1.lower() == string2.lower():
+    print("Рядки однакові")
+else:
+    print("Рядки різні")
+
+german_word = 'straße'  # В нижньому регістрі
+search_word = 'STRASSE'  # В верхньому регістрі
+
+# Порівняння за допомогою lower()
+lower_comparison = german_word.lower() == search_word.lower()
+
+# Порівняння за допомогою casefold()
+casefold_comparison = german_word.casefold() == search_word.casefold()
+
+print(f"Порівняння з lower(): {lower_comparison}")
+print(f"Порівняння з casefold(): {casefold_comparison}")
