d4baba7bf5d671cf272c14c1164992d16568642b
Dimanerter <erterujamp@gmail.com>
2024-09-30 15:31:30 +0200

    utf-16, cp1251

--- working_with_files/part1/main.py
+++ working_with_files/part1/main.py
@@ -74,3 +74,33 @@ print(s[1])
 
 byte_str = 'some text'.encode()
 print(byte_str)
+
+
+# Перетворення чисел у байт-рядки
+
+numbers = [0, 128, 255]
+byte_numbers = bytes(numbers)
+print(byte_numbers)
+
+for num in [127, 255,156]:
+    print(hex(num))
+
+
+# Кодування рядків (ASCII, UTF-8, CP1251)
+
+print(ord('%'))
+print(chr(1000))
+
+s = "Привіт!"
+
+utf8 = s.encode()
+print(f"UTF-8: {utf8}")
+
+utf16 = s.encode('utf-16')
+print(f"UTF-16: {utf16}")
+
+cp1251 = s.encode('cp1251')
+print(f"CP-1251: {cp1251}")
+
+s_from_utf16 = utf16.decode("utf-16")
+print(s_from_utf16 == s)
