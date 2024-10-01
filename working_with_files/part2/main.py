5af809ec7a46b202b005a2212a73974a915b4d09
Dimanerter <erterujamp@gmail.com>
2024-10-01 19:36:57 +0200

    write_text

--- working_with_files/part2/main.py
+++ working_with_files/part2/main.py
@@ -22,3 +22,35 @@ relative_path = Path("documents/example.txt")
 absolute_path = relative_path.absolute()
 print(absolute_path)
 
+original_path = Path("documents/example.txt")
+
+new_path = original_path.with_name("report.txt")
+print(new_path)
+
+original_path = Path("documents/example.txt")
+new_path = original_path.with_suffix(".md")
+print(new_path)
+
+print(original_path)
+
+# original_path = Path("C:/Users/avdee/Documents/example.txt")
+
+# new_path = original_path.with_name("report.txt")
+# original_path.rename(new_path)
+
+
+# -------------------------------Читання та запис файлів--------------------
+file_path = Path("example.txt")
+
+file_path.write_text("Привіт світ!", encoding = "utf-8")
+
+
+
+
+
+
+
+
+
+
+
